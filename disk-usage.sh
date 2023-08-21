b2b1b2fcc48f448465dea0f061d76775694c7fd7
venkat10118 <venkat.adgu@gmail.com>
2023-08-21 14:06:51 +0530

    added script

---
+++ disk-usage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+DISK_USAGE=$(df -ht | grep -vE 'tmpfs|Filesystem')
+disk_usage_threshold=1
+#IFS= internal field separator is space
+while IFS= read line
+do 
+    echo "output: $line"
+done <<< $DISK_USAGE
